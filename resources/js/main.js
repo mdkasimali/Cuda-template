e6c75f81c2117efefc8148be072ccdcb591d3bc2
mdkasimali <kasimalik4846@gmail.com>
2021-02-07 09:19:47 +0600

    cuda template

---
+++ resources/js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    $(".js--services-section").waypoint(function(direction){
+        if(direction == "down"){
+            $("nav").addClass("sticky");
+        } else {
+            $("nav").removeClass("sticky");
+        }
+
+    });
+    $("nav ul li a").click(function(){
+        $("nav ul li a").removeClass("active");
+        $(this).addClass("active");
+    });
+
+    $("nav a img.logo").click(function(){
+        $("nav ul li a").removeClass("active");
+        $("nav ul li:first-child a").addClass("active");
+    });
+
+    //mixitup
+    var mixer = mixitup('.container');
+});
+
+function openNav(){
+    document.getElementById("myNav").style.width = "100%";
+}
+function closeNav(){
+    document.getElementById("myNav").style.width = "0";
+}
